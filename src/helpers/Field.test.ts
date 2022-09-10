0e59d25bc4f5650672ff67421ea1bb63c34314a9
Serhii Trachuk <s.trachuk@apiko.com>
2022-09-10 23:36:40 +0300

    Finish core module & js doc.

--- src/helpers/Field.test.ts
+++ src/helpers/Field.test.ts
@@ -1,43 +1,45 @@
-import {emptyFieldGenerator, CellState, fieldGenerator} from './Field'
+import {
+  generateFieldWithDefaultState,
+  fieldGenerator,
+  CellState,
+  Cell,
+} from './Field';
 
-const {empty, hidden, bomb} = CellState
+const { empty, bomb, hidden } = CellState;
 
-describe('Field generator', function () {
-  describe('emptyFieldGeneratorTest', function () {
-    it('should generate 2x2', function () {
-      expect(emptyFieldGenerator(2)).toEqual([
+const cellWithBombFilter = (cell: Cell) => cell === bomb;
+
+describe('Field Generator', () => {
+  describe('emptyFieldGenerator tests', () => {
+    it('2x2', () => {
+      expect(generateFieldWithDefaultState(2)).toStrictEqual([
         [empty, empty],
         [empty, empty],
-      ])
+      ]);
     });
-    it('should generate 3x3', function () {
-      expect(emptyFieldGenerator(3)).toEqual([
+    it('3x3', () =>
+      expect(generateFieldWithDefaultState(3)).toStrictEqual([
         [empty, empty, empty],
         [empty, empty, empty],
         [empty, empty, empty],
-      ])
-    });
-    it('should generate 3x3 with hidden state', function () {
-      expect(emptyFieldGenerator(3, hidden)).toEqual([
+      ]));
+    it('3x3 with hidden state', () =>
+      expect(generateFieldWithDefaultState(3, hidden)).toStrictEqual([
         [hidden, hidden, hidden],
         [hidden, hidden, hidden],
         [hidden, hidden, hidden],
-      ])
-    });
+      ]));
   });
-
-  describe('fieldGenerator', function () {
-    it('should throw error for wrong probability', function () {
-      const errorText = 'Probability must be between 0 and 1'
-      expect(() => fieldGenerator(1, -1)).toThrow(errorText)
-      expect(() => fieldGenerator(1, 2)).toThrow(errorText)
+  describe('Simple cases', () => {
+    it('Wrong dencity', () => {
+      const errorText = 'Probability must be between 0 and 1';
+      expect(() => fieldGenerator(1, -1)).toThrow(errorText);
+      expect(() => fieldGenerator(1, 2)).toThrow(errorText);
     });
-
-    it('should return valid value for smallest possible field without mine', function () {
-      expect(fieldGenerator(1, 0)).toStrictEqual([[empty]])
+    it('Smallest possible field without mine', () => {
+      expect(fieldGenerator(1, 0)).toStrictEqual([[empty]]);
     });
-
-    it('should return valid value for 10x10 field without mine', function () {
+    it('Big field without mine', () => {
       expect(fieldGenerator(10, 0)).toStrictEqual([
         [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
         [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
@@ -49,29 +51,41 @@ describe('Field generator', function () {
         [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
         [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
         [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
-      ])
+      ]);
     });
-
-    it('should return valid value for smallest possible field with mine', function () {
-      expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]])
+    it('Smallest possible field with mine', () => {
+      expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]]);
     });
-
-    it('should return valid value for 2x2 field with mine', function () {
+    it('2x2 field with mines', () => {
       expect(fieldGenerator(2, 1)).toStrictEqual([
         [bomb, bomb],
         [bomb, bomb],
-      ])
+      ]);
     });
+    it('2x2 field with 50% probability', () => {
+      const field = fieldGenerator(2, 0.5);
+      const flatField = field.flat();
 
-    it('should return valid value for 2x2 field with 50% probability', function () {
-      const field = fieldGenerator(2, .5)
-      const flatField = field.flat()
+      const cellsWithBombs = flatField.filter(cellWithBombFilter);
+      const emptyCells = flatField.filter((cell: Cell) => cell === 2);
 
-      const cellsWithBombs = flatField.filter(cell => cell === bomb)
-      const emptyCells = flatField.filter(cell => cell === empty)
+      expect(cellsWithBombs).toHaveLength(2);
+      expect(emptyCells).toHaveLength(2);
+    });
+    it('Real game field size = 10x10 with 1/4 mined cells (25 mines)', () => {
+      const size = 10;
+      const mines = 25;
 
-      expect(cellsWithBombs).toHaveLength(2)
-      expect(emptyCells).toHaveLength(2)
+      const probability = mines / (size * size);
+      const field = fieldGenerator(size, probability);
+
+      const flatField = field.flat();
+
+      expect([...field[0], ...field[1]].join('')).not.toBe(
+        '99999999999999999999'
+      );
+
+      expect(flatField.filter(cellWithBombFilter)).toHaveLength(mines);
     });
   });
 });
